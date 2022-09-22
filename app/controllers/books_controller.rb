9d900231132f4438c077d14a4f70d7d76cc4ce01
lisanyny <110669469+lisanyny@users.noreply.github.com>
2022-09-22 10:18:00 +0000

    [Update]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,8 +5,15 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @books = Book.all
     @book.user_id = current_user.id
-    @book.save
+    if @book.save
+      flash[:notice] = "You have create book successfully."
+      redirect_to books_path
+    else
+      @user = current_user
+      render :index
+    end
   end
 
   def edit
@@ -15,17 +22,25 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+       flash[:notice] = "You have updated book successfully."
+       redirect_to book_path(@book.id)
+    else
+      @user = current_user
+      render :edit
+    end
+
   end
 
   def index
     @books = Book.all
     @user = current_user
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
+    @newbook = Book.new
     @user = current_user
   end
 
